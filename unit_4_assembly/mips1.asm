a294190557f50bde4c3cc8d851ce3c0de7471a07
Jacky <balenodevelopment@gmail.com>
2016-05-31 18:42:15 -0400

    add new question files and started on jumps.txt

--- unit_4_assembly/mips1.asm
+++ unit_4_assembly/mips1.asm
@@ -1,4 +1,8 @@
-li $3, 0xB2D05E00
-li $2, 23
-divu $3, $2
-mfhi $13 
+li $3, 0xaa000000
+li $4, 0x00bb0000
+li $5, 0x0000ff00
+li $6, 0
+bgtz $6, foo
+foo: add $3, $4, $5
+sub $3, $4, $5
+add $4, $4, $6
