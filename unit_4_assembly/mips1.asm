74f162acbab96b86a7c3337a03ef2faf7f604b61
Jacky <balenodevelopment@gmail.com>
2016-06-02 16:45:05 -0400

    fix assembly jump question

--- unit_4_assembly/mips1.asm
+++ unit_4_assembly/mips1.asm
@@ -1,8 +1,10 @@
-li $3, 0xaa000000
-li $4, 0x00bb0000
-li $5, 0x0000ff00
-li $6, 0
-bgtz $6, foo
-foo: add $3, $4, $5
-sub $3, $4, $5
-add $4, $4, $6
+li $3, 55 # this is variable x
+li $4, 0  # this is variable y
+li $5, 29
+li $6, -2
+sub $7, $3, $5 # this result will be used to compare to zero
+bltz $7, foo
+li $3, 35
+foo: mult $3, $6
+mfhi $4
+mflo $4
